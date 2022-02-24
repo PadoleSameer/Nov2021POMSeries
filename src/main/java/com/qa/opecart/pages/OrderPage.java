63d751808e358353e2206dc02e5eff13116c8673
Padole <spadole@dxc.com>
2022-02-24 16:27:47 +0530

    Updated order page as per review

--- src/main/java/com/qa/opecart/pages/OrderPage.java
+++ src/main/java/com/qa/opecart/pages/OrderPage.java
@@ -1,8 +1,12 @@
 package com.qa.opecart.pages;
 
+import org.openqa.selenium.By;
+
 public class OrderPage {
 
+	By order = By.id("order");
+	
 	public void order() {
-		System.out.println("order");
+		System.out.println("order id = 12345");
 	}
 }
